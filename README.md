177a4779c4f808e8ff1a7ba94c9ec68f8de7414a
UlquiorraCiferR4 <skanderbouafif8@gmail.com>
2026-02-21 21:58:34 +0100

    update

---
+++ README.md
@@ -0,0 +1,8 @@
+### Hello There : 
+
+
+## Map : 
+
+- problemes is where I'post all my code 
+from leetcode  to any prob solving platform 
+
--- app/fgets.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main (){
-  char text[100];
-  printf("give me ur name :");
-  fgets(text,sizeof(text),stdin);
-  printf("Hello Mr %s",text);
-  return 0;
-}
